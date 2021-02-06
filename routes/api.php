ebf3835e3d5992cb43b97d6993f51905082c22b8
anderrf <ander_rochafarias@live.com>
2021-02-06 14:51:39 -0300

    model Usuario

--- routes/api.php
+++ routes/api.php
@@ -14,6 +14,22 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:api')->get('/user', function (Request $request) {
+/*Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
+});*/
+
+Route::prefix('v1')->group(function(){
+    Route::get('lista', function(){
+        return ["a", "b", "c", "d"];
+    });
+
+    Route::post('cadastra', function(){
+        echo 'implementar';
+    });
 });
+
+/*Route::prefix('v2')->group(function(){
+    //
+});*/
+
+
--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'App\Http\Controllers\Usuario@cadastrar');
+Route::post('/salvar', 'App\Http\Controllers\Usuario@salvar');
+
+//Route::prefix('user')->group();
